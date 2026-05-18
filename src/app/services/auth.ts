a7d31b739e80fbb0f08737e11ec15a9e602114c6
Selma Karacic <selmakaracic@MacBook-Pro-od-Selma.local>
2026-05-18 23:52:49 +0200

    Fix auth service to use environment apiUrl

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map, catchError, of } from 'rxjs';
 import { User } from '../models/user';
+import { environment } from '../../environments/environment';
 
 interface BackendUser {
   id: number | string;
@@ -19,7 +20,7 @@ interface AuthResult {
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly apiUrl = 'http://localhost:8080/api/users';
+  private readonly apiUrl = `${environment.apiUrl}/api/users`;
   private readonly storageKey = 'parksmart_user';
 
   constructor(private http: HttpClient) {}
